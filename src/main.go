63d8343bb305212205116ca4c11ee932a0af9a50
mitch <robertsmj1138@gmail.com>
2020-08-02 16:06:20 -0400

    get rid of the module and add some test replay parses

--- src/main.go
+++ src/main.go
@@ -3,45 +3,78 @@ package main
 import (
 	"log"
 	"os"
+	"path/filepath"
 
 	"github.com/dotabuff/manta"
 	"github.com/dotabuff/manta/dota"
 )
 
 func main() {
-	f, err := os.Open("out/replay.dem")
-	if err != nil {
-		log.Fatalf("unable to open file: %s", err)
-	}
-	defer f.Close()
+	// Create a new parser instance from a file. Alternatively see NewParser([]byte)
+	var files []string
+	err := filepath.Walk("replays", func(path string, info os.FileInfo, err error) error {
+		// skip folders
+		if info.IsDir() {
+			return nil
+		}
 
-	finfo, err := f.Stat()
-	if err != nil {
-		log.Fatalf("File stat error: %s", err)
-	}
-	log.Printf("filename: %v", finfo)
-
-	p, err := manta.NewStreamParser(f)
-	if err != nil {
-		log.Fatalf("unable to create parser: %s", err)
-	}
-
-	p.Callbacks.OnCUserMessageSayText(func(m *dota.CUserMessageSayText) error {
-		log.Printf("%d said: %s\n", m.GetPlayerindex(), m.GetText())
+		files = append(files, path)
 		return nil
 	})
+	if err != nil {
+		log.Fatal("error walking replay dir")
+	}
 
-	p.Callbacks.OnCUserMessageSayText2(func(m *dota.CUserMessageSayText2) error {
-		log.Printf("%s said: %s\n", m.GetParam1(), m.GetParam2())
-		return nil
-	})
+	log.Print("starting")
+	for _, file := range files {
+		log.Printf("replay: %s", file)
+		f, err := os.Open(file)
+		if err != nil {
+			log.Fatalf("unable to open file: %s", err)
+		}
+		defer f.Close()
 
-	p.Callbacks.OnCDOTAUserMsg_AbilityPing(func(ping *dota.CDOTAUserMsg_AbilityPing) error {
-		log.Printf("%d pinged %d\n", ping.GetCasterId(), ping.GetAbilityId())
-		return nil
-	})
+		p, err := manta.NewStreamParser(f)
+		if err != nil {
+			log.Fatalf("unable to create parser: %s", err)
+		}
 
-	p.Start()
+		// Register a callback, this time for the OnCUserMessageSayText2 event.
+		p.Callbacks.OnCUserMessageSayText2(func(m *dota.CUserMessageSayText2) error {
+			log.Printf("%s said: %s\n", m.GetParam1(), m.GetParam2())
+			return nil
+		})
 
-	log.Printf("finished parsing\n")
+		// Register a callback for map pings
+		p.Callbacks.OnCDOTAUserMsg_LocationPing(func(ping *dota.CDOTAUserMsg_LocationPing) error {
+			location := ping.GetLocationPing()
+			log.Printf("%d pinged at (%d, %d)", ping.GetPlayerId(), location.GetX(), location.GetY())
+			return nil
+		})
+
+		// Never called?
+		p.Callbacks.OnCDOTAUserMsg_AbilityPing(func(ping *dota.CDOTAUserMsg_AbilityPing) error {
+			log.Printf("%d ability-pinged %d", ping.GetPlayerId(), ping.GetAbilityId())
+			return nil
+		})
+
+		// Never called?
+		p.Callbacks.OnCDOTAUserMsg_ItemAlert(func(itemalert *dota.CDOTAUserMsg_ItemAlert) error {
+			alert := itemalert.GetItemAlert()
+			log.Printf("%d item-alerted %d", itemalert.GetPlayerId(), alert.GetItemAbilityId())
+			return nil
+		})
+
+		// Never called?
+		p.Callbacks.OnCDOTAUserMsg_EnemyItemAlert(func(itemalert *dota.CDOTAUserMsg_EnemyItemAlert) error {
+			log.Printf("%d enemy-item-alerted %d's %d", itemalert.GetPlayerId(), itemalert.GetTargetPlayerId(), itemalert.GetItemAbilityId())
+			return nil
+		})
+
+		p.Start()
+
+		log.Printf("parse complete\n")
+
+	}
+
 }
